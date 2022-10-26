5a6ae311700ac88117d0e045cbe1446f5e635da8
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2022-10-27 00:01:56 +0200

    Initial Commit

---
+++ FitnessHeadTracker/FitnessHeadTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessHeadTrackerApp.swift
+//  FitnessHeadTracker
+//
+//  Created by Dennis Moschina on 27.10.22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessHeadTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
