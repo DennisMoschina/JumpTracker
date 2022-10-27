431be4dc25a4dd7c85f7dc2b42e7aa4073c589ee
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2022-10-27 14:02:22 +0200

    added initializers

--- FitnessHeadTracker/model/data/motion/Acceleration.swift
+++ FitnessHeadTracker/model/data/motion/Acceleration.swift
@@ -11,4 +11,10 @@ struct Acceleration {
     var x: Double
     var y: Double
     var z: Double
+    
+    init(x: Double = 0, y: Double = 0, z: Double = 0) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
 }
