e2ef8cc9087a2dfbcd75187364733ade304ca2f3
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2022-12-02 22:11:03 +0100

    fixed Edit button showing in wrong view

--- FitnessHeadTracker/view/HomeView.swift
+++ FitnessHeadTracker/view/HomeView.swift
@@ -28,5 +28,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
